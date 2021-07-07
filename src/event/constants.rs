bd044b026202fc37e01375b57db88ed95f92265c
Tim <tmaloney@pdx.edu>
2021-07-07 15:50:39 -0700

    Refactor: changed macros to const fn

--- src/event/constants.rs
+++ src/event/constants.rs
@@ -4,54 +4,60 @@
 //! here vary by architecture, and that these constants
 //! are produced by `bindgen`. These macros are used to
 //! define the constants for use in perf-related ioctls.
-
 include!("../bindings/perf_event.rs");
+use std::mem::size_of;
 
-macro_rules! ioc {
-    ($dir:expr, $ty:expr, $nr:expr, $sz:expr) => {
-        (($dir as u32) << _IOC_DIRSHIFT)
-            | (($ty as u32) << _IOC_TYPESHIFT)
-            | (($nr as u32) << _IOC_NRSHIFT)
-            | (($sz as u32) << _IOC_SIZESHIFT)
-    };
+/// '$' is the ioctl number for `linux/perf_event`.
+/// For more information on ioctl numbers
+/// see `Linux ioctls' reference in `README.md`.
+const _IO_TYPE: u32 = b'$' as u32;
+
+/// Neither read nor write to kernel
+const fn iocn(nr: u32) -> u32 {
+    // no direction,
+    // no size.
+    let sz: usize = 0;
+    (_IOC_NONE << _IOC_DIRSHIFT)
+        | (_IO_TYPE << _IOC_TYPESHIFT)
+        | (nr << _IOC_NRSHIFT)
+        | ((sz as u32) << _IOC_SIZESHIFT)
 }
 
-macro_rules! io {
-    ($ty:expr, $nr:expr) => {
-        ioc!(_IOC_NONE, $ty, $nr, 0)
-    };
+/// User: write
+/// Kernel: read
+const fn iocw(nr: u32, sz: usize) -> u32 {
+    (_IOC_WRITE << _IOC_DIRSHIFT)
+        | (_IO_TYPE << _IOC_TYPESHIFT)
+        | (nr << _IOC_NRSHIFT)
+        | ((sz as u32) << _IOC_SIZESHIFT)
 }
 
-macro_rules! ior {
-    ($ty:expr, $nr:expr, $sz:expr) => {
-        ioc!(_IOC_READ, $ty, $nr, $sz)
-    };
+/// User: read
+/// Kernel: write
+const fn iocr(nr: u32, sz: usize) -> u32 {
+    (_IOC_WRITE << _IOC_DIRSHIFT)
+        | (_IO_TYPE << _IOC_TYPESHIFT)
+        | (nr << _IOC_NRSHIFT)
+        | ((sz as u32) << _IOC_SIZESHIFT)
 }
 
-macro_rules! iow {
-    ($ty:expr, $nr:expr, $sz:expr) => {
-        ioc!(_IOC_WRITE, $ty, $nr, $sz)
-    };
+/// Kernel: reads and writes
+const fn iocwr(nr: u32, sz: usize) -> u32 {
+    ((_IOC_READ | _IOC_WRITE) << _IOC_DIRSHIFT)
+        | (_IO_TYPE << _IOC_TYPESHIFT)
+        | (nr << _IOC_NRSHIFT)
+        | ((sz as u32) << _IOC_SIZESHIFT)
 }
 
-macro_rules! iowr {
-    ($ty:expr, $nr:expr, $sz:expr) => {
-        ioc!(_IOC_READ | _IOC_WRITE, $ty, $nr, $sz)
-    };
-}
-
-///
-pub const PERF_EVENT_IOC_ENABLE: u32 = io!(b'$', 0);
-pub const PERF_EVENT_IOC_DISABLE: u32 = io!(b'$', 0);
-pub const PERF_EVENT_IOC_REFRESH: u32 = io!(b'$', 2);
-pub const PERF_EVENT_IOC_RESET: u32 = io!(b'$', 3);
-pub const PERF_EVENT_IOC_PERIOD: u32 = iow!('$', 4, std::mem::size_of::<libc::__u64>());
-pub const PERF_EVENT_IOC_SET_OUTPUT: u32 = io!(b'$', 5);
-pub const PERF_EVENT_IOC_SET_FILTER: u32 = iow!(b'$', 6, std::mem::size_of::<*const char>());
-pub const PERF_EVENT_IOC_ID: u32 = ior!(b'$', 7, std::mem::size_of::<*const u64>());
-pub const PERF_EVENT_IOC_SET_BPF: u32 = iow!(b'$', 8, std::mem::size_of::<libc::__u32>());
-pub const PERF_EVENT_IOC_PAUSE_OUTPUT: u32 = iow!(b'$', 9, std::mem::size_of::<libc::__u32>());
-pub const PERF_EVENT_IOC_QUERY_BPF: u32 =
-    iowr!(b'$', 10, std::mem::size_of::<*const perf_event_query_bpf>());
-pub const PERF_EVENT_IOC_MODIFY_ATTRIBUTES: u32 =
-    iowr!(b'$', 11, std::mem::size_of::<*const perf_event_attr>());
+pub const PERF_EVENT_IOC_ENABLE: u32 = iocn(0);
+pub const PERF_EVENT_IOC_DISABLE: u32 = iocn(1);
+pub const PERF_EVENT_IOC_REFRESH: u32 = iocn(2);
+pub const PERF_EVENT_IOC_RESET: u32 = iocn(3);
+pub const PERF_EVENT_IOC_PERIOD: u32 = iocw(4, size_of::<u64>());
+pub const PERF_EVENT_IOC_SET_OUTPUT: u32 = iocn(5);
+pub const PERF_EVENT_IOC_SET_FILTER: u32 = iocw(6, size_of::<*const char>());
+pub const PERF_EVENT_IOC_ID: u32 = iocr(7, size_of::<*const u64>());
+pub const PERF_EVENT_IOC_SET_BPF: u32 = iocw(8, size_of::<u32>());
+pub const PERF_EVENT_IOC_PAUSE_OUTPUT: u32 = iocw(9, size_of::<u32>());
+pub const PERF_EVENT_IOC_QUERY_BPF: u32 = iocwr(10, size_of::<*const perf_event_query_bpf>());
+pub const PERF_EVENT_IOC_MODIFY_ATTRIBUTES: u32 = iocwr(11, size_of::<*const perf_event_attr>());
