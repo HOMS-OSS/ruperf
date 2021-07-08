bf6d5b70cb3ddc6d0b68fd4be6b9e98c7f7e362f
Sam Little <kagutaba256@gmail.com>
2021-07-08 14:51:48 -0700

    test added to cli, 3 basic tests including libpfm4 test

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@
 
 mod event;
 mod stat;
+mod test;
 mod utils;
 
 use event::*;
 use stat::*;
+use test::*;
 extern crate structopt;
 
 use structopt::StructOpt;
@@ -19,19 +21,27 @@ use structopt::StructOpt;
 /// Define command line options.
 #[derive(Debug, StructOpt)]
 enum Opt {
-    #[structopt(
+  #[structopt(
         setting = structopt::clap::AppSettings::TrailingVarArg,
         setting = structopt::clap::AppSettings::AllowLeadingHyphen,
         name = "stat",
         about = "Collects hardware/software event counters"
     )]
-    Stat(StatOptions),
+  Stat(StatOptions),
+  #[structopt(
+        setting = structopt::clap::AppSettings::TrailingVarArg,
+        setting = structopt::clap::AppSettings::AllowLeadingHyphen,
+        name = "test",
+        about = "Runs sanity tests"
+    )]
+  Test(TestOptions),
 }
 
 fn main() {
-    let opt = Opt::from_args();
-    match opt {
-        Opt::Stat(x) => run_stat(&x),
-    }
-    perf_event_hello();
+  let opt = Opt::from_args();
+  match opt {
+    Opt::Stat(x) => run_stat(&x),
+    Opt::Test(x) => run_test(&x),
+  }
+  perf_event_hello();
 }
