cf551d2f09200c63eef4a359283cc8a103bd2559
Sam Little <kagutaba256@gmail.com>
2021-07-12 17:58:56 -0700

    Feat: CLI path for perf test

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@
 
 mod event;
 mod stat;
+mod test;
 mod utils;
 
 use event::*;
 use stat::*;
+use test::*;
 extern crate structopt;
 
 use structopt::StructOpt;
@@ -26,12 +28,20 @@ enum Opt {
         about = "Collects hardware/software event counters"
     )]
     Stat(StatOptions),
+    #[structopt(
+        setting = structopt::clap::AppSettings::TrailingVarArg,
+        setting = structopt::clap::AppSettings::AllowLeadingHyphen,
+        name = "test",
+        about = "Runs sanity tests"
+    )]
+    Test(TestOptions),
 }
 
 fn main() {
     let opt = Opt::from_args();
     match opt {
         Opt::Stat(x) => run_stat(&x),
+        Opt::Test(x) => run_test(&x),
     }
     perf_event_hello();
 }
---
+++ src/test/pfm.rs
@@ -0,0 +1,27 @@
+use crate::test::Test;
+use std::process::Command;
+
+// TEST: Check for presence of libpfm4
+pub fn test_check_for_libpfm4() -> Test {
+    // This uses the linux command "ldconfig" and returns
+    // based on whether or not the output contains "libpfm."
+    fn check_for_libpfm4() -> bool {
+        let output = Command::new("ldconfig")
+            .args(&["-p"])
+            .output()
+            .expect("Issue running command.");
+        if output.status.success() {
+            let text = String::from_utf8_lossy(&output.stdout);
+            if text.contains("libpfm") {
+                return true;
+            }
+        }
+        false
+    }
+
+    Test {
+        name: "has_libpfm4".to_string(),
+        description: "Checks for presence of libpfm4".to_string(),
+        call: check_for_libpfm4,
+    }
+}
