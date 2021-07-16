1c4a1bd6b0aa389bbc2b67fa9cdac5915ddaa7b4
Greg Hairfield <ghairfield@gmail.com>
2021-07-16 11:13:50 -0700

    Fix: Cleaned up commit history - Added CLI argument for iterations of test programs - file-io now uses '/tmp' - Removed Makefile

--- examples/file-io.rs
+++ examples/file-io.rs
@@ -3,30 +3,41 @@
 //! This is a small program to run ruperf against. Write and read a file. This 
 //! should call `syscall_write` and `syscall_read`. 
 //! 
-//! The test environment should not need to be a create and should only include
-//! std::* libs.
-//!
-//! Run `make clean` to remove the file.
-//!
 //! Usage:
 //!     ruperf stat ./file-io
-use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
-use std::path::Path;
+use std::path::PathBuf;
+use std::env;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "File-io", about = "A test program to run ruperf against")]
+struct Opt {
+    /// Number of write to a file
+    #[structopt(short, long, default_value = "100000")]
+    count: usize,
+}
 
 static WRITE_STRING: &str = "The quick brown fox jumped over the lazy dog\n";
 
 fn main() {
-    let writes = 10000;
-    let path = Path::new("read-write-test.txt");
+    let opt = Opt::from_args();
+    let writes = opt.count;
+    let tmp_dir = env::temp_dir();
+    let mut path = PathBuf::from(tmp_dir);
+    path.push("ruperf-fileio-test");
+    path.set_extension("txt");
 
-    // Create a file and write to it.
     if path.exists() {
-        println!("Test read/write path exists. If this is a mistake, run `make clean`");
-        return    
+        match fs::remove_file(&path) {
+            Err(e) => panic!("Could not remove existing file. {} because {}", path.display(), e),
+            Ok(_) => (),
+        }
     }
 
-    let mut file = match File::create(&path) {
+    // Create a file and write to it.
+    let mut file = match fs::File::create(&path) {
         Err(e) => panic!("Could not create file {} because {}", path.display(), e),
         Ok(file) => file,
     };
@@ -42,7 +53,7 @@ fn main() {
     drop(file);
 
     // Open same file and read from it.
-    let mut file = match File::open(&path) {
+    let mut file = match fs::File::open(&path) {
         Err(e) => panic!("Could not open the file because {}", e),
         Ok(file) => file,
     };
