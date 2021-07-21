5827a3453b1f2a16a286da6b04651876ac01a323
Briana Oursler <briana.oursler@gmail.com>
2021-07-21 09:16:50 -0700

    feat: Create module for bindings
    Update includes to isolate style warn ignores and change references to
    use new module instead of using fd for includes.
    
    Signed-off-by: Briana Oursler <briana.oursler@gmail.com>

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -2,11 +2,6 @@
 //! `perf_event_open()` see /src/bindings/perf_event.rs.
 
 //! Disable cargo build warnings created due to using bindgen
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(dead_code)]
-
 pub mod event;
 mod fd;
 mod sys;
@@ -19,13 +14,14 @@ pub fn perf_event_hello() {
 #[cfg(test)]
 #[test]
 fn wrapper_test() {
-    let sample_struct = fd::perf_event_attr__bindgen_ty_1 { sample_period: 1 };
-    let event = &mut fd::perf_event_attr {
-        type_: fd::perf_type_id_PERF_TYPE_HARDWARE,
-        size: std::mem::size_of::<fd::perf_event_attr>() as u32,
-        config: fd::perf_hw_id_PERF_COUNT_HW_INSTRUCTIONS as u64,
+    use crate::bindings::*;
+    let sample_struct = perf_event_attr__bindgen_ty_1 { sample_period: 1 };
+    let event = &mut perf_event_attr {
+        type_: perf_type_id_PERF_TYPE_HARDWARE,
+        size: std::mem::size_of::<perf_event_attr>() as u32,
+        config: perf_hw_id_PERF_COUNT_HW_INSTRUCTIONS as u64,
         __bindgen_anon_1: sample_struct,
-        sample_type: fd::perf_event_sample_format_PERF_SAMPLE_IP,
+        sample_type: perf_event_sample_format_PERF_SAMPLE_IP,
         ..Default::default()
     };
     event.set_disabled(1);
