81477d86f0399b366d3e87bf1fc665cfa05d0112
Briana Oursler <briana.oursler@gmail.com>
2021-07-22 22:56:58 -0700

    Fix: Add mod.rs file for bindings
    Fix PR 59 by adding mod.rs file. File was ignored by .gitignore.
    
    Signed-off-by: Briana Oursler <briana.oursler@gmail.com>

---
+++ src/bindings/mod.rs
@@ -0,0 +1,8 @@
+#![allow(unknown_lints)]
+#![allow(clippy::all)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+include!("./perf_event.rs");
