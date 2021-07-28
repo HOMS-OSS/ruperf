44c3f030cd5c0a3d9ea747309aedc27a3f839ce9
Briana Oursler <briana.oursler@gmail.com>
2021-07-27 19:16:31 -0700

    fix: Start counter for each event and run program once
    Rather than running program for each event counter, start each counter while
    child is SIGSTOP and then stop each counter after child is finished. Print
    outcome of each counter.
    
    Signed-off-by: Briana Oursler <briana.oursler@gmail.com>

--- src/stat.rs
+++ src/stat.rs
@@ -26,6 +26,16 @@ impl FromStr for StatEvent {
     }
 }
 
+/// Match on each supported event to parse from command line
+impl ToString for StatEvent {
+    fn to_string(&self) -> String {
+        match self {
+            StatEvent::Cycles => "cycles".to_string(),
+            StatEvent::Instructions => "instructions".to_string(),
+        }
+    }
+}
+
 /// Configuration settings for running stat
 #[derive(Debug, StructOpt)]
 pub struct StatOptions {
@@ -41,31 +51,47 @@ pub struct StatOptions {
 /// Run perf stat on the given command and event combinations. Currently starts and stops a cycles timer in serial for each event specified.
 pub fn run_stat(options: &StatOptions) {
     //demonstrating from cli. In future rather than starting and stopping counter in series for each event, events will have the ability to be added in groups that will coordinate their timing.
+    struct EventCounter {
+        event: Event,
+        start: isize,
+        stop: isize,
+    }
 
-    for command in &options.command {
-        for event in &options.event {
-            let mut child = Command::new(&options.command[0])
-                .args(&options.command[1..])
-                .spawn()
-                .unwrap();
-            //prevent race condition on child program run time
-            unsafe { kill(child.id() as i32, libc::SIGSTOP) };
-            let e = Event::new(*event, Some(&child));
-            let cnt: isize = e.start_counter().unwrap();
-            unsafe { kill(child.id() as i32, libc::SIGCONT) };
+    let mut event_list: Vec<EventCounter> = Vec::new();
+    let mut child = Command::new(&options.command[0])
+        .args(&options.command[1..])
+        .spawn()
+        .unwrap();
+    //prevent race condition on child program run time on most programs
+    unsafe { kill(child.id() as i32, libc::SIGSTOP) };
+    for event in &options.event {
+        let e = Event::new(*event, Some(&child));
+        let start = e.start_counter().unwrap();
+        event_list.push(EventCounter {
+            event: e,
+            start: start,
+            stop: 0,
+        });
+    }
+    unsafe { kill(child.id() as i32, libc::SIGCONT) };
 
-            //create another process from command
-            child.wait().expect("Failed to execute process");
+    //create another process from command
+    child.wait().expect("Failed to execute process");
 
-            let final_cnt = e.stop_counter().unwrap();
-            let total_cnt = final_cnt - cnt;
+    for e in event_list.iter_mut() {
+        e.stop = e.event.stop_counter().unwrap();
+    }
 
-            //output command's output
-            println!(
-                "Performance counter stats for '{}'\n",
-                options.command.get(0).unwrap()
-            );
-            println!(" Number of cycles: {}\n", total_cnt);
-        }
+    //output command's output
+    println!(
+        "Performance counter stats for '{}'\n",
+        options.command.get(0).unwrap()
+    );
+    for event in event_list {
+        println!(
+            " Number of {}: {}\n",
+            event.event.event.to_string(),
+            event.stop - event.start
+        );
     }
 }
