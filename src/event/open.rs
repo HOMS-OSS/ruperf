a45a5df89d676a46b4585244c0b79c71d4abbea0
Briana Oursler <briana.oursler@gmail.com>
2021-07-28 22:16:05 -0700

    feat: use threads and ipc to avoid race condition
    note: not working, can't get pipe to set up in parent and then read from child
    in parent. Various comments show previous attempts and point out where the
    problems are as well as attempt to document what I'm trying to do.
    
    Signed-off-by: Briana Oursler <briana.oursler@gmail.com>

--- src/event/open.rs
+++ src/event/open.rs
@@ -31,9 +31,9 @@ pub fn event_open(event: &StatEvent) -> Result<perf_event_attr, EventErr> {
 }
 impl Event {
     /// Construct a new event
-    pub fn new(event: StatEvent, child: Option<&std::process::Child>) -> Self {
+    pub fn new(event: StatEvent, pid: Option<u32>) -> Self {
         let e: &mut perf_event_attr = &mut event_open(&event).unwrap();
-        let fd = fd::FileDesc::new(e, child, -1, -1);
+        let fd = fd::FileDesc::new(e, pid, -1, -1);
         Self { fd, event }
     }
 
