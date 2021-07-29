b949babc1194c302b112e20c5f73f9429bec6d49
Sam Little <kagutaba256@gmail.com>
2021-07-29 03:19:06 -0700

    Feat: added --json flag to print perf test as json

--- src/test/testutils.rs
+++ src/test/testutils.rs
@@ -3,6 +3,8 @@ use crate::test::pfm;
 use crate::test::RunSettings;
 use crate::test::Test;
 use crate::test::TestResult;
+use serde_json::json;
+use serde_json::to_string_pretty;
 use std::io::stdout;
 use std::io::Write;
 /// Gathers all tests and returns a Vec with them all
@@ -20,9 +22,48 @@ pub fn make_tests() -> Vec<Test> {
 /// Runs all tests and outputs results to stdout
 pub fn run_all_tests(tests: &[Test], to_skip: &[String], settings: &RunSettings) {
     let mut should_skip;
+    let mut tests_passed = 0;
+    let mut tests_failed = 0;
+    let mut tests_skipped = 0;
+    let mut results_as_json: Vec<serde_json::Value> = Vec::new();
+    if settings.json {}
     for (index, test) in tests.iter().enumerate() {
         should_skip = to_skip.iter().any(|i| *i == index.to_string());
-        run_single_test(&test, index, should_skip, "".to_string(), &settings);
+        let result = run_single_test(&test, index, should_skip, "".to_string(), &settings);
+        if settings.json {
+            let result_string: String;
+            match result {
+                TestResult::Passed => {
+                    tests_passed += 1;
+                    result_string = String::from("passed");
+                }
+                TestResult::Failed(_) => {
+                    tests_failed += 1;
+                    result_string = String::from("failed");
+                }
+                TestResult::Skipped => {
+                    tests_skipped += 1;
+                    result_string = String::from("skipped");
+                }
+            }
+            results_as_json.push(json!({
+                "name": test.name,
+                "description": test.description,
+                "result": result_string,
+                "number": index as u32
+            }));
+        }
+    }
+    if settings.json {
+        let result_object = json!({
+            "tests_available": results_as_json.len(),
+            "tests_ran": results_as_json.len() - tests_skipped,
+            "tests_passed": tests_passed,
+            "tests_failed": tests_failed,
+            "tests_skipped": tests_skipped,
+            "results": results_as_json,
+        });
+        println!("{}", to_string_pretty(&result_object).unwrap());
     }
 }
 
@@ -34,21 +75,24 @@ pub fn run_single_test(
     parent_index_string: String,
     settings: &RunSettings,
 ) -> TestResult {
-    print!(
-        "{:>2}{}: {:<60} : ",
-        parent_index_string, index, test.description
-    );
-    stdout().flush().unwrap();
+    if !settings.json {
+        print!(
+            "{:>2}{}: {:<60} : ",
+            parent_index_string, index, test.description
+        );
+        stdout().flush().unwrap();
+    }
     let result_type: TestResult;
     if should_skip {
         result_type = TestResult::Skipped;
     } else if test.subtests.is_empty() {
         result_type = (test.call)(&settings);
     } else {
-        println!();
+        if !settings.json {
+            println!();
+        }
         let mut overall_result_type: TestResult = TestResult::Passed;
         for (i, subtest) in test.subtests.iter().enumerate() {
-            // TODO: change false to a given subtest skip
             let result = run_single_test(subtest, i, false, index.to_string() + ".", settings);
             if let TestResult::Failed(_) = result {
                 overall_result_type = TestResult::Failed(String::new());
@@ -57,12 +101,14 @@ pub fn run_single_test(
         result_type = overall_result_type;
         return result_type;
     }
-    let result_text: String = match &result_type {
-        TestResult::Skipped => "\x1b[0;33mSkip\x1b[0m".to_string(),
-        TestResult::Passed => "Ok".to_string(),
-        TestResult::Failed(s) => format!("\x1b[0;31mFAILED!\x1b[0m {}", s),
-    };
-    println!("{}", result_text);
+    if !settings.json {
+        let result_text: String = match &result_type {
+            TestResult::Skipped => "\x1b[0;33mSkip\x1b[0m".to_string(),
+            TestResult::Passed => "Ok".to_string(),
+            TestResult::Failed(s) => format!("\x1b[0;31mFAILED!\x1b[0m {}", s),
+        };
+        println!("{}", result_text);
+    }
     result_type
 }
 
